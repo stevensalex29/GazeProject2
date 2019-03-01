b54cb365e0c0a3eec8e85c08e5151aa118e5b61a
stevensalex29 <stevensalex29@gmail.com>
2019-03-01 13:51:21 -0500

    added UI

---
+++ The Mines of Srigimor/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Play game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // How To Play
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    // Main Menu
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Quit
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
